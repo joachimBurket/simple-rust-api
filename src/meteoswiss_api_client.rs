1840f705dbb2856d13107765faaaebe69b995385
Joachim Burket <joachim.burket@protonmail.ch>
2021-06-05 12:02:02 +0000

    adding meteoswiss API client

---
+++ src/meteoswiss_api_client.rs
@@ -0,0 +1,109 @@
+
+
+use serde::Deserialize;
+use csv::ReaderBuilder;
+
+/// The measures taken on a specific station at a specific time
+#[derive(Debug, Deserialize)]
+pub struct MeasuringPoint {
+    // Station shortname
+    #[serde(rename(deserialize = "Station/Location"))]
+    station: String,
+
+    // Measure datetime yyyyMMddHHmm
+    #[serde(rename(deserialize = "Date"))]
+    datetime: String,
+
+    // Air temperature 2m above ground in °C (current value)
+    #[serde(rename(deserialize = "tre200s0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    temp: Option<f32>,
+
+    // Precipitations in mm (10min total)
+    #[serde(rename(deserialize = "rre150z0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    precipitations: Option<f32>,
+
+    // Sunshine duration in min (10min total)
+    #[serde(rename(deserialize = "sre000z0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    sunshine: Option<f32>,
+    
+    // Global radiation in W/m2 (10min mean)
+    #[serde(rename(deserialize = "gre000z0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    radiation: Option<f32>,
+    
+    // Relative air humidity 2m above ground (current value)
+    #[serde(rename(deserialize = "ure200s0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    humidity: Option<f32>,
+    
+    // Dew point 2m above ground (current value)
+    #[serde(rename(deserialize = "tde200s0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    dew_point: Option<f32>,
+    
+    // Wind direction in ° (10min mean)
+    #[serde(rename(deserialize = "dkl010z0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    wind_direction: Option<f32>,
+    
+    // Wind speed in km/h (10min mean)
+    #[serde(rename(deserialize = "fu3010z0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    wind_speed: Option<f32>,
+    
+    // Wind gust (one second) peak in km/h (maximum)
+    #[serde(rename(deserialize = "fu3010z1"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    wind_gust_peak: Option<f32>,
+    
+    // Pressure at station level in hPa (current value)
+    #[serde(rename(deserialize = "prestas0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    pressure: Option<f32>,
+    
+    // Pressure reduced at see level in hPa (current value)
+    #[serde(rename(deserialize = "pp0qffs0"))]
+    #[serde(deserialize_with = "csv::invalid_option")]
+    pressure_at_see_level: Option<f32>,
+}
+
+/// Client to query the MeteoSwiss API
+pub struct MeteoSwissApiClient {
+    base_url: String,
+}
+
+impl MeteoSwissApiClient {
+    const CSV_DELIMITER: u8 = b';';
+
+    // Constructor
+    fn new(base_url: String) -> MeteoSwissApiClient {
+        MeteoSwissApiClient {
+            base_url,
+        }
+    }
+
+    /// Returns the last measures of the network's stations
+    fn get_last_measures(&self) -> Result<(), Box<dyn std::error::Error>> { // Result<Vec<MeasuringPoint>, Box<dyn std::error::Error>> {
+        let resp = reqwest::blocking::get(&self.base_url)?;
+        let status = resp.status();
+        if status.is_success() {
+            let txt = resp.text()?;
+            let mut reader = ReaderBuilder::new().delimiter(MeteoSwissApiClient::CSV_DELIMITER)
+                .from_reader(txt.as_bytes());
+            
+            let mut measuring_points: Vec<MeasuringPoint> = Vec::new();
+
+            for result in reader.deserialize::<MeasuringPoint>() {
+                let result = result?;
+                println!("{:?}", result);
+                measuring_points.push(result);
+            }
+            // Ok(measuring_points)
+        } 
+        println!("Oh no :( Something bad happened.. Status: {:?}", status);
+        Ok(())
+    }
+}
